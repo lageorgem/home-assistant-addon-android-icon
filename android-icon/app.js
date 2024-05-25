dbc30b557b95a86e47c6640feaff847ce6a73c40
George Mihai <lazugeorgem@gmail.com>
2024-05-25 18:49:45 +0300

    first commit

---
+++ android-icon/app.js
@@ -0,0 +1,50 @@
+const axios = require('axios');
+const scrapy = require('node-scrapy')
+const fs = require('fs');
+const path = require('path');
+const cors = require('cors');
+
+const express = require('express')
+const app = express()
+
+app.use(cors());
+
+app.get('/app/:id', async function (req, res) {
+    const imagePath = path.resolve(__dirname, `./downloads/${req.params.id}.png`);
+
+    // Check if the image already exists
+    if (fs.existsSync(imagePath)) {
+        const imageBuffer = fs.readFileSync(imagePath);
+
+        res.setHeader('Content-Type', 'image/png');
+        res.setHeader('Content-Disposition', `inline;filename="${req.params.id}.png"`);
+        res.setHeader('Cache-Control', 'public, max-age=86400, no-transform');
+        return res.send(imageBuffer);
+    }
+
+    const url = `https://play.google.com/store/apps/details?id=${req.params.id}`
+    const model = "img[alt='Icon image'] (src)"
+
+    const page = await axios.get(url);
+    let imageSrc = scrapy.extract(page.data, model)
+
+    if (!imageSrc) {
+        return res.status(404).send('Image not found');
+    }
+
+    imageSrc = imageSrc.replace(/=.+$/, '');
+
+    const image = await axios.get(imageSrc, { responseType: 'arraybuffer' });
+
+    // Save the image buffer to the file system
+    fs.writeFileSync(imagePath, image.data);
+
+    res.setHeader('Content-Type', 'image/png');
+    res.setHeader('Content-Disposition', `inline;filename="${req.params.id}.png"`)
+    res.setHeader('Cache-Control', 'public, max-age=86400, no-transform')
+    res.send(image.data)
+})
+
+app.listen(1337, () => {
+    console.log(`Server is running on port 1337`);
+})
---
+++ android-icon/run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+node /usr/src/app/app.js
